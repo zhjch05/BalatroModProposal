e105a18164a71f8788389231326c3875b0aff4d4
James Zhang <zhjch05@live.com>
2024-06-04 13:22:12 +0800

    hide seed

--- Hide.lua
+++ Hide.lua
@@ -6,12 +6,22 @@
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
+
+-- Hide Steam Mods MODS button
 local create_UIBox_main_menu_buttonsRef = create_UIBox_main_menu_buttons
 function create_UIBox_main_menu_buttons()
 	local menu = create_UIBox_main_menu_buttonsRef()
 	table.remove(menu.nodes[1].nodes[1].nodes, #menu.nodes[1].nodes[1].nodes)
-	menu.nodes[1].nodes[1].config = {align = "cm", padding = 0.15, r = 0.1, emboss = 0.1, colour = G.C.L_BLACK, mid = true}
 	return(menu)
+end
+
+-- Hide Seed within run
+local Orginal_create_UIBox_generic_options = create_UIBox_generic_options
+function create_UIBox_generic_options(args)
+  if  #args==0 and args.contents and #args.contents>=2 and G.STAGE == G.STAGES.RUN then
+    table.remove(args.contents, 2)
+  end
+  return Orginal_create_UIBox_generic_options(args)
 end
 
 ----------------------------------------------
